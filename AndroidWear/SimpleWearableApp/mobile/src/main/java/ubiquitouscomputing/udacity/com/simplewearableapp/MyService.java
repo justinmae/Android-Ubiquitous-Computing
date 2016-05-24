ff37eac0329c095d26bffdc1228e7efe56284367
Justin Mae <justin511@gmail.com>
2016-05-23 21:28:28 -0400

    Add simple wearable app with DataItem to send data from wearable to phone

---
+++ AndroidWear/SimpleWearableApp/mobile/src/main/java/ubiquitouscomputing/udacity/com/simplewearableapp/MyService.java
@@ -0,0 +1,25 @@
+package ubiquitouscomputing.udacity.com.simplewearableapp;
+
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class MyService extends WearableListenerService {
+
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEventBuffer) {
+        for (DataEvent dataEvent : dataEventBuffer) {
+            if (dataEvent.getType() == DataEvent.TYPE_CHANGED) {
+                DataMap dataMap = DataMapItem.fromDataItem(dataEvent.getDataItem()).getDataMap();
+                String path = dataEvent.getDataItem().getUri().getPath();
+                if (path.equals("/step-counter")) {
+                    int steps = dataMap.getInt("step-count");
+                    long time = dataMap.getLong("timestamp");
+                }
+            }
+        }
+    }
+}
---
+++ AndroidWear/SimpleWearableApp/wear/src/main/java/ubiquitouscomputing/udacity/com/simplewearableapp/MainActivity.java
@@ -0,0 +1,79 @@
+package ubiquitouscomputing.udacity.com.simplewearableapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+    private TextView mTextView;
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+    public void sendStepCount(int steps, int timestamp) {
+        PutDataMapRequest putDataMapRequest = PutDataMapRequest.create("/step-counter");
+
+        putDataMapRequest.getDataMap().putInt("step-count", steps);
+        putDataMapRequest.getDataMap().putLong("timestamp", timestamp);
+
+        PutDataRequest request = putDataMapRequest.asPutDataRequest();
+
+        Wearable.DataApi.putDataItem(mGoogleApiClient, request)
+                .setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
+                    @Override
+                    public void onResult(@NonNull DataApi.DataItemResult dataItemResult) {
+                        if (!dataItemResult.getStatus().isSuccess()) {
+                            // failed to send step count data item
+                        } else {
+                            // successfully sent step count data item
+                        }
+                    }
+                });
+    }
+}
